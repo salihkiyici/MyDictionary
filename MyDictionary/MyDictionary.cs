5fbb80283bf553577f5fc8c11172cde84937f564
salihkiyici <salihkiyici852@gmail.com>
2021-04-06 11:38:42 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<K, V>
+    {
+        K[] _key;
+        V[] _value;
+
+        K[] _tempKey;
+        V[] _tempValue;
+        public MyDictionary()
+        {
+            _key = new K[0];
+            _value = new V[0];
+        }
+        public void Add(K key, V value)
+        {
+            _tempKey = _key;
+            _tempValue = _value;
+            _key = new K[_key.Length + 1];
+            _value = new V[_value.Length + 1];
+
+
+            for (int i = 0; i < _tempKey.Length; i++)
+            {
+                _key[i] = _tempKey[i];
+            }
+
+            for (int i = 0; i < _tempValue.Length; i++)
+            {
+                _value[i] = _tempValue[i];
+            }
+
+
+            _key[_key.Length - 1] = key;
+            _value[_value.Length - 1] = value;
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> students1 = new Dictionary<int, string>();
+            students1.Add(123, "Salih");
+            students1.Add(235, "Metin");
+
+            MyDictionary<int, string> students2 = new MyDictionary<int, string>();
+            students2.Add(1653, "Yiğit");
+            students2.Add(3425, "Ahmet");
+        }
+    }
+}
